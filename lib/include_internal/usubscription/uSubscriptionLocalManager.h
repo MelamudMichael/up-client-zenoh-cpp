b6254f79881f17ec1a0419156097acc7f22f4c93
Michael Melamud <melamudmichael@gmail.com>
2024-03-03 15:27:25 +0000

    usub

--- lib/include_internal/usubscription/uSubscriptionLocalManager.h
+++ lib/include_internal/usubscription/uSubscriptionLocalManager.h
@@ -1,16 +1,43 @@
 
+/*
+ * Copyright (c) 2023 General Motors GTO LLC
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * SPDX-FileType: SOURCE
+ * SPDX-FileCopyrightText: 2023 General Motors GTO LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+ 
 #ifndef _H_USUBSCRIPTION_LOCAL_MANAGER_
 #define _H_USUBSCRIPTION_LOCAL_MANAGER_
 
 #include <unordered_map>
+
 #include <ustatus.pb.h>
+#include <up-client-zenoh-cpp/transport/zenohUTransport.h>
 #include <core/usubscription/v3/usubscription.pb.h>
 
 using namespace std;
+using namespace uprotocol::utransport;
 using namespace uprotocol::core::usubscription::v3;
 using namespace uprotocol::v1;
 
-class SubscriptionLocalManager {
+class SubscriptionLocalManager : public UListener {
 
     public:
 
@@ -35,6 +62,10 @@ class SubscriptionLocalManager {
     private:
     
         SubscriptionLocalManager() {};
+
+        UStatus onReceive(const UUri& uri,
+                          const UPayload& payload,
+                          const UAttributes& attributes) const override;
 
         unordered_map<std::string, UCode> pubStatusMap_;
         unordered_map<std::string, SubscriptionStatus_State> subStatusMap_;
--- lib/src/uSubscriptionClient.cpp
+++ lib/src/uSubscriptionClient.cpp
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2023 General Motors GTO LLC
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * SPDX-FileType: SOURCE
+ * SPDX-FileCopyrightText: 2023 General Motors GTO LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <up-client-zenoh-cpp/rpc/zenohRpcClient.h>
 #include <up-client-zenoh-cpp/usubscription/uSubscriptionClient.h>
 #include <up-client-zenoh-cpp/usubscription/uSubscriptionCommon.h>
@@ -270,6 +294,10 @@ UStatus SubscriptionLocalManager::init() {
     
     UStatus status;
 
+    ZenohUTransport::instance().init();
+
+  //  ZenohUTransport::instance().registerListener(uSubUpdateUri, SubscriptionLocalManager::instance());
+    
     status.set_code(UCode::OK);
     
     return status;
@@ -278,6 +306,8 @@ UStatus SubscriptionLocalManager::init() {
 UStatus SubscriptionLocalManager::term() {
     
     UStatus status;
+
+    ZenohUTransport::instance().term();
 
     status.set_code(UCode::OK);
     
@@ -348,10 +378,16 @@ UCode SubscriptionLocalManager::getPublisherStatus(const UUri &uri) {
     }
 }
 
-UCode getPublisherStatus(const UUri &uri) {
-   return SubscriptionLocalManager::instance().getPublisherStatus(uri);
-}
+UStatus SubscriptionLocalManager::onReceive(const UUri& uri,
+                                            const UPayload& payload,
+                                            const UAttributes& attributes) const {
+    (void)uri;
+    (void)payload;
+    (void)attributes;
 
-SubscriptionStatus_State getSubscriberStatus(const UUri &uri) {
-    return SubscriptionLocalManager::instance().getSubscriptionStatus(uri);
+    UStatus status;
+
+    status.set_code(UCode::OK);
+
+    return status;
 }
